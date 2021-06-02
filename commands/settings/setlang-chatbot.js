bf0289b3a309da8893da4bfd991d04e0596ef14b
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-02 04:47:01 +0000

    💘🍝 Checkpoint ./commands/settings/setlang-chatbot.js:24282868/3282

---
+++ commands/settings/setlang-chatbot.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  name: "setlang",
+  category: "settings",
+  permissions: "ADMINISTRATOR",
+  usage: "setlang <lang>",
+  description: "Set language For command chat-bot",
+  botPermission: ['VIEW_CHANNEL','EMBED_LINKS','ATTACH_FILES','MANAGE_CHANNELS','MANAGE_GUILD'],
+  authorPermission: ['VIEW_CHANNEL','EMBED_LINKS','ATTACH_FILES','MANAGE_CHANNELS','MANAGE_GUILD'],
+  run: (client, message, args) => {
+  const langs = ["afrikaans", "albanian", "amharic", "arabic", "armenian", "azerbaijani", "bangla", "basque", "belarusian", "bengali", "bosnian", "bulgarian", "burmese", "catalan", "cebuano", "chichewa", "corsican", "croatian", "czech", "danish", "dutch", "english", "esperanto", "estonian", "filipino", "finnish", "french", "frisian", "galician", "georgian", "german", "greek", "gujarati", "haitian creole", "hausa", "hawaiian", "hebrew", "hindi", "hmong", "hungarian", "icelandic", "igbo", "indonesian", "irish", "italian", "japanese", "javanese", "kannada", "kazakh", "khmer", "korean", "kurdish (kurmanji)", "kyrgyz", "lao", "latin", "latvian", "lithuanian", "luxembourgish", "macedonian", "malagasy", "malay", "malayalam", "maltese", "maori", "marathi", "mongolian", "myanmar (burmese)", "nepali", "norwegian", "nyanja", "pashto", "persian", "polish", "portugese", "punjabi", "romanian", "russian", "samoan", "scottish gaelic", "serbian", "sesotho", "shona", "sindhi", "sinhala", "slovak", "slovenian", "somali", "spanish", "sundanese", "swahili", "swedish", "tajik", "tamil", "telugu", "thai", "turkish", "ukrainian", "urdu", "uzbek", "vietnamese", "welsh", "xhosa", "yiddish", "yoruba", "zulu"];
+  if(!args[0]) return client.send(`Please enter the language that will be sent by the chat-bot \n${langs.map((l, i) => `#${i+1} - ${l}`).join("\n")}`, message)
+  if(!langs.includes(language)){client.send("Error: No Language Not Invaild")
+    
+  client.data.set(`LANG_${message.guild.id}`)
+  }
+};
