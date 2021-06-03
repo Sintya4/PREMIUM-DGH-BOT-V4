ab6892f1bf86e9ff72070fe3aa4e5038437a4c0a
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-03 12:02:41 +0000

    🏟👹 Checkpoint ./commands/YouTube-Studio/addapi.js:24282868/449

---
+++ commands/YouTube-Studio/addapi.js
@@ -0,0 +1,21 @@
+module.exports = {
+        name: "addapi",
+        usage: `addapi [Channel_id_youtuber]`,
+        category: "youtube",
+        description: "",
+        args: true,
+        cooldown: 2,
+    run: async (client, message, args) => {
+//code
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+}}
