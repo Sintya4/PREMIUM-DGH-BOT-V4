1f04b1a64d8e9c2b4567984419722f8230bb468f
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-04 12:10:20 +0000

    🏯🛳 Checkpoint ./server.js:26780611/3254

--- server.js
+++ server.js
@@ -160,6 +160,44 @@ client.on("messageDelete", function(message, channel) {
         .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
     }
   });
+  client.on("message", async message => {
+    if (message.author.bot || !message.guild || message.webhookID) return;
+    let status = db.get(`afkstatus_${message.guild.id}_${message.author.id}`);
+    let reason;
+    if (status === true) {
+      db.set(`afkstatus_${message.guild.id}_${message.author.id}`, false);
+      db.delete(`afk_${message.guild.id}_${message.author.id}`);
+      message.member.setNickname(message.author.username).catch(err => {});
+      return client.send(`**Welcome Back ${message.author}**`, message);
+    }
+    if (status === message.author.id) {
+      db.set(`afkstatus_${message.guild.id}_${message.author.id}`, false);
+      db.delete(`afk_${message.guild.id}_${message.author.id}`);
+      message.member.setNickname(message.author.username).catch(err => {});
+      return client.send(`**Welcome Back ${message.author}**`, message);
+    }
+    if (message.mentions.users.size) {
+      let mentions = message.mentions.users;
+      mentions = mentions.filter(mention => mention.id !== message.author.id);
+      if (mentions.size) {
+        let victim = mentions.find(mention =>
+          db.get(`afk_${message.guild.id}_${mention.id}`)
+        );
+        if (victim) {
+          status = db.get(`afkstatus_${message.guild.id}_${victim.id}`);
+          reason = db.get(`afk_${message.guild.id}_${victim.id}`);
+          let time = db.get(`time_${message.guild.id}_${victim.id}`);
+          time = Date.now() - time;
+          return client.send(
+            `**${victim.username} is currently AFK - ${reason} - ${format(
+              time
+            )} ago**`,
+            message
+          );
+        }
+      }
+    }
+ });
   //<SETUP>
   client.on("message", async message => {
     if (message.author.bot || !message.guild || message.webhookID) return;
@@ -188,42 +226,7 @@ client.on("messageDelete", function(message, channel) {
     let command =
       client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
     if (!command) return;
-   let status = db.get(`afkstatus_${message.guild.id}_${message.author.id}`);
-    let reason;
-    if (status === true) {
-      db.set(`afkstatus_${message.guild.id}_${message.author.id}`, false);
-      db.delete(`afk_${message.guild.id}_${message.author.id}`);
-      message.member.setNickname(message.author.username).catch(err => {});
-      return client.send(`**Welcome Back ${message.author}**`, message);
-    }
-  /*  if (status(message.content)) {
-      db.set(`afkstatus_${message.guild.id}_${message.author.id}`, false);
-      db.delete(`afk_${message.guild.id}_${message.author.id}`);
-      message.member.setNickname(message.author.username).catch(err => {});
-      return client.send(`**Welcome Back ${message.author}**`, message);
-    }*/
-    if (message.mentions.users.size) {
-      let mentions = message.mentions.users;
-      mentions = mentions.filter(mention => mention.id !== message.author.id);
-      if (mentions.size) {
-        let victim = mentions.find(mention =>
-          db.get(`afk_${message.guild.id}_${mention.id}`)
-        );
-        if (victim) {
-          status = db.get(`afkstatus_${message.guild.id}_${victim.id}`);
-          reason = db.get(`afk_${message.guild.id}_${victim.id}`);
-          let time = db.get(`time_${message.guild.id}_${victim.id}`);
-          time = Date.now() - time;
-          return client.send(
-            `**${victim.username} is currently AFK - ${reason} - ${format(
-              time
-            )} ago**`,
-            message
-          );
-        }
-      }
-    }
- 
+   
     if (command.enabled === false) {
       const embed = new Discord.MessageEmbed()
         .setDescription(`This command is disabled.`)
