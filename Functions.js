5afc1b6a238ac85d7c6875d0aa5b3a992846c2bb
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-07 09:07:25 +0000

    💅🎍 Checkpoint ./server.js:26780611/2787 ./Functions.js:26780611/2924

--- Functions.js
+++ Functions.js
@@ -99,6 +99,108 @@ async awaitReply(message, question, limit = 60000, obj = false) {
     if (len === 0) return '';
     if (len === 1) return arr[0];
     return `${arr.slice(0, -1).join(', ')}${len > 1 ? `${len > 2 ? ',' : ''} ${conj} ` : ''}${arr.slice(-1)}`;
+  },
+  async randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  async getRandomString(length) {
+    var chars =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    var s = "";
+    for (var i = 0; i < length; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      s += chars.substring(rnum, rnum + 1);
+    }
+
+    return s;
+  },
+  async send(content, message, color) {
+    if (!color) color = "RANDOM";
+
+    return message.channel.send({
+      embed: { description: content, color: color }
+    });
+  },
+  async emo(count) {
+    const mapping = {
+      " ": "  ",
+      "0": ":zero:",
+      "1": ":one:",
+      "2": ":two:",
+      "3": ":three:",
+      "4": ":four:",
+      "5": ":five:",
+      "6": ":six:",
+      "7": ":seven:",
+      "8": ":eight:",
+      "9": ":nine:"
+    };
+    let coun = count
+      .split("")
+      .map(c => mapping[c] || c)
+      .join("");
+    return coun;
+  },
+  async text(text) {
+    const emojified = `${text}`
+      .toLowerCase()
+      .split("")
+      .map(letter => {
+        if (/[a-z]/g.test(letter)) {
+          return `:regional_indicator_${letter}: `;
+        }
+
+        return letter;
+      })
+      .join("");
+    return emojified;
+  },
+  async resolveUser(search) {
+    if (!search || typeof search !== "string") return null;
+    let user = null;
+    if (search.match(/^<@!?(\d+)>$/))
+      user = this.users.fetch(search.match(/^<@!?(\d+)>$/)[1]).catch(() => {});
+    if (search.match(/^!?(\w+)#(\d+)$/) && !user)
+      user = this.users.cache.find(
+        u =>
+          u.username === search.match(/^!?(\w+)#(\d+)$/)[0] &&
+          u.discriminator === search.match(/^!?(\w+)#(\d+)$/)[1]
+      );
+    if (search.match(/.{2,32}/) && !user)
+      user = this.users.cache.find(u => u.username === search);
+    if (!user) user = this.users.fetch(search).catch(() => {});
+    return user;
+  },
+ async formating(ms) {
+  let days, daysms, hours, hoursms, minutes, minutesms, sec;
+  days = Math.floor(ms / (24 * 60 * 60 * 1000));
+  daysms = ms % (24 * 60 * 60 * 1000);
+  hours = Math.floor(daysms / (60 * 60 * 1000));
+  hoursms = ms % (60 * 60 * 1000);
+  minutes = Math.floor(hoursms / (60 * 1000));
+  minutesms = ms % (60 * 1000);
+  sec = Math.floor(minutesms / 1000);
+
+  return (
+    days +
+    " Days, " +
+    hours +
+    " Hours, " +
+    minutes +
+    " Minutes, " +
+    sec +
+    " Seconds."
+  );
+},
+ async translate(text, message) {
+    let language = await client.data.get(`LANG_${message.guild.id}`);
+    let translate = require("@k3rn31p4nic/google-translate-api");
+    const translated = await translate(text, {
+      to: language || "english"
+    });
+    return translated.text;
   }
 
 };
