474c966355552801fd578955c268ba2703fa0ecd
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-12 05:42:40 +0000

    🍤🙎 Checkpoint ./commands/settings/nqn.js:26780611/42

--- commands/settings/nqn.js
+++ commands/settings/nqn.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "nqn",
   usage: "nqn <on/off>",
-  description: "add guild custom commands",
-  category: "admin",
+  description: "",
+  category: "settings",
   args: true,
   authorPermission: ["MANAGE_MESSAGES"],
   botPermission: ["MANAGE_MESSAGES"],
