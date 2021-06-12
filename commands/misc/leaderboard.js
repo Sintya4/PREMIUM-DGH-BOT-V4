f1a6230f585bedcc3f412e08d323d01ec78e853a
Glitch (indecisive-lumbar-cuckoo) <none>
2021-06-12 04:01:55 +0000

    🏤⛵️ Checkpoint ./commands/misc/leaderboard.js:26780611/1136

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -1,13 +1,24 @@
 const { MessageEmbed } = require("discord.js");
 const db = require("quick.db");
 const fs = require("fs");
+const Levels = require("discord-xp");
 module.exports = {
-  name: "leaderb",
+  name: "leaderboard",
+  aliases: ["lb"],
   category: "misck",
   botPermission: ["MANAGE_GUILD"],
-
   run: async (client, message, args) => {
-    const coins = await client.db
+  const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+if (rawLeaderboard.length < 1) return message.reply("Nobody's in leaderboard yet.");
+const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
+const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
+   const embed = new MessageEmbed()
+    .setTitle(`**Leaderboard ${message.guild.name}**`)
+     .setColor("#efcb83")
+    .setDescription(`${lb.join("\n===================\n\n")}`);
+   message.channel.send(embed) 
+    
+   }} /* const coins = await client.db
       .all()
       .filter(data => data.ID.startsWith(`guild_${message.guild.id}_xp_`))
       .sort((a, b) => b.data - a.data);
@@ -42,3 +53,4 @@ module.exports = {
     message.inlineReply(embed);
   }
 };
+*/
