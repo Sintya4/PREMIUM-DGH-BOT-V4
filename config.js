d435609bedcee9b201fd4b85abfbb272fd3d609b
Sintya <81017780+Sintya4@users.noreply.github.com>
2021-07-13 10:35:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
-exports.Token = ["TOKEN BOT"]; //Bot Token - Important
-exports.Owner = "767726828311543820"; //Bot Owner ID - Important
-exports.Developer = "740947753135243354"; //Bot Dev ID - Important
-exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
-exports.COLOR = "BLUE";
+exports.Token = ["TOKEN BOT","TOKEN BOT#2"]///ot Token - Important
+exports.Owner = "Owner ID; //Bot Owner ID - Important
+exports.Developer = "Dev ID; //Bot Dev ID - Important
+exports.Default_Prefix = "!"//Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = "https://dsc.gg/mincoder"; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
-exports.Dashboard = "https://bot-jsll.glitch.me/"; //Dashboard Your Bot
-exports.Server_ID = "826826587592065095"; // ID Your Server
-exports.mongodb =
-  "MONGODB URL";
-exports.QUEUE_LIMIT = 0;
+exports.Support = "LINK SERVER; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Dashboard = "WEBSITE BOT; //Dashboard Your Bot
+exports.Server_ID = "SERVER FOR PREMIUM COMMANDS; // ID Your Server
+exports.mongodb = "MONGODB";
