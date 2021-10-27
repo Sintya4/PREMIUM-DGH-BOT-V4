42f77971d95588a7edfc552f743b5a84aa30e21c
Sintya <81017780+Sintya4@users.noreply.github.com>
2021-10-27 10:35:10 +0700

    ឵

--- src/interaction/buttons/Ticket/ticket-1.js
+++ src/interaction/buttons/Ticket/ticket-1.js
@@ -4,6 +4,8 @@ module.exports = {
     const chs = interaction.channel;
     let id = client.db.get(`${chs.id}_ticket`);
     let MSG = await chs.messages.fetch(id);
+    let user = await client.data.get(`ticket_user_${chs.id}`);
+    client.data.delete(`ticket_close_${user}`);
     MSG.delete().catch(e => {});
   }
 };
