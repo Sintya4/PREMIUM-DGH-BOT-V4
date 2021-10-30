3a0b7feaf12253a552a8ac08cac49f75671049b7
Sintya <81017780+Sintya4@users.noreply.github.com>
2021-10-30 19:09:37 +0700

    update Bot-add-remove.js

--- src/events/Guild/Bot-add-remove.js
+++ src/events/Guild/Bot-add-remove.js
@@ -1,63 +1,61 @@
 module.exports = async client => {
   client.on("guildCreate", async guild => {
-    let newserverEmbedw = new client.Discord.MessageEmbed()
+    const owner = await guild.fetchOwner();
+    const embed = new client.Discord.MessageEmbed()
+      .setColor("#5DBCD2")
+      .setTitle("New Server")
+      .addField("Name", `${guild.name}`, true)
+      .addField("Guild ID", `${guild.id}`, true)
+      .addField("Owner", `${owner.user.tag}`, true)
+      .addField("Owner ID", `${owner.user.id}`, true)
+      .addField("Users", `${guild.members.cache.size}`, true)
+      .setFooter(`Guilds: ${client.guilds.cache.size}`)
+      .setThumbnail(guild.iconURL());
+    const embed_ = new client.Discord.MessageEmbed()
       .setTitle(`Hello, I'm ${client.user.username}!`)
       .setDescription(
         client.config.bot.bot_add_description.split("{guild}").join(guild.name)
       )
       .setColor("#5DBCD2")
       .setImage(client.config.image.guild_add);
-    let embed = new client.Discord.MessageEmbed()
-      .setTitle("Guild Add")
-      .setDescription(
-        `Name Server: ${guild.name}\nMember: ${
-          guild.memberCount
-        }\nOwner: ${await client.resolveUser(guild.ownerId).then(x => x.tag)}`
-      )
-      .setColor("#ff0000")
-      .setThumbnail(guild.iconURL({ dynamic: true }))
-      .setFooter(guild.name, guild.iconURL({ dynamic: true }));
     client.sendhook(null, {
       channel: client.config.logs.botadd,
       embed: [embed],
       name: "DGH ADD GUILD"
     });
-    return client
-      .resolveUser(guild.ownerId)
-      .then(user => user.send({ embeds: [newserverEmbedw] }))
-      .catch(() => {
-        null;
-      });
+    return owner.send({ embeds: [embed_] }).catch(() => {
+      null;
+    });
   });
+
   client.on("guildDelete", async guild => {
-    if (!guild.available) return;
-    let newserverEmbedw = new client.Discord.MessageEmbed()
+    const owner = await guild.fetchOwner();
+
+    const embed = new client.Discord.MessageEmbed()
+      .setColor("#5DBCD2")
+      .setTitle("Server Left")
+      .addField("Name", `${guild.name}`, true)
+      .addField("Guild ID", `${guild.id}`, true)
+      .addField("Owner", `${owner.user.tag}`, true)
+      .addField("Owner ID", `${owner.user.id}`, true)
+      .addField("Users", `${guild.members.cache.size}`, true)
+      .setFooter(`Guilds: ${client.guilds.cache.size}`)
+      .setThumbnail(guild.iconURL());
+    const embed_ = new client.Discord.MessageEmbed()
       .setDescription(
         client.config.bot.bot_remove_description
           .split("{guild}")
           .join(guild.name)
       )
-      .setColor("#5DBCD2");
-    let embed = new client.Discord.MessageEmbed()
-      .setTitle("Guild Remove")
-      .setDescription(
-        `Name Server: ${guild.name}\nMember: ${
-          guild.memberCount
-        }\nOwner: ${await client.resolveUser(guild.ownerId).then(x => x.tag)}`
-      )
-      .setColor("#ff0000")
-      .setThumbnail(guild.iconURL({ dynamic: true }))
-      .setFooter(guild.name, guild.iconURL({ dynamic: true }));
+      .setColor("#5DBCD2")
+      .setImage(client.config.image.guild_remove);
     client.sendhook(null, {
       channel: client.config.logs.botdel,
       embed: [embed],
       name: "DGH REMOVE GUILD"
     });
-    return client
-      .resolveUser(guild.ownerId)
-      .then(user => user.send({ embeds: [newserverEmbedw] }))
-      .catch(() => {
-        null;
-      });
+    return owner.send({ embeds: [embed_] }).catch(() => {
+      null;
+    });
   });
 };
